5d76349bbdde0c223e12db0b38216c6c119fd674
Sukirthalakshmanan <sukirtha3700@gmail.com>
2022-10-21 09:39:39 +0530

    My first project

---
+++ JenkinsProject/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsProject
+{
+    public class Class1
+    {
+        int i = 10;
+        int j=20;
+        public void Add()
+        {
+            int ans = i + j;
+        }
+
+        public void Sub()
+        {
+            int ans = i - j;
+        }
+
+        public void Multiply()
+        {
+            int ans = i * j;
+        }
+    }
+}
